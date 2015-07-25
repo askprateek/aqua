585a778436df8e391a665d9bb873deda006d6739
Vipin Khushu <vipinkhushu@hotmail.com>
2015-07-26 03:07:24 +0530

    Major

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-  <title>Waterworks</title>
+  <title>AQUA - The Home Water Delivery System</title>
 
   <!-- Bootstrap -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
@@ -24,13 +24,34 @@
 
   <nav>
     <div class="nav-wrapper white">
-      <a href="#" class="brand-logo center" style="color:#00BCD4;">Waterworks</a>
+      <a href="#" class="brand-logo center" style="color:#00BCD4;">AQUA</a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href ="vendor/login.php" style="color:#00BCD4;">Vender Login</a></li>
-        <li><a href ="vendor/signup.php" style="color:#00BCD4;">Vender Signup</a></li>
+        <li><a href ="vendor/login.php" style="color:#00BCD4;">Vendor Login</a></li>
+        <li><a href ="vendor/signup.php" style="color:#00BCD4;">Vendor Signup</a></li>
       </ul>
     </div>
   </nav>
+  <?php
+if(isset($_GET["msg"]))
+{
+	if($_GET["msg"]=="err")
+	{
+echo"<div class='card-panel red red'>Invalid Email/ Password</div>
+";
+	}
+	else
+	{
+	echo"
+	
+	    <div class='card-panel teal green'>Your Have been Registered Successfully, Now Login To Proceed</div>
+
+	";	
+	}
+	
+}
+
+
+?>
   <div class="container">
     <div class="row">
       <div class="col s10 col offset-s1 valign wrapper">
@@ -40,7 +61,7 @@
 
             <div class="row">
               <div class="input-field col s12">
-                <input id="email" type="email" name="email" class="validate">
+                <input id="email" type="email" name="username" class="validate">
                 <label for="email">Email</label>
               </div>
             </div>
@@ -70,6 +91,7 @@
       <div class="modal-content">
         <h4 style="color:#00BCD4;">Register Here</h4>
         <div class="row">
+		<form action="reg.php" method="post">
           <div class="input-field col s12">
             <input id="email" type="text" name="name" class="validate">
             <label for="email">Full Name</label>
@@ -80,6 +102,12 @@
           <div class="input-field col s12">
             <input id="email" type="email" name="email" class="validate">
             <label for="email">Email</label>
+          </div>
+        </div>
+		 <div class="row">
+          <div class="input-field col s12">
+            <input id="email" type="email" name="phone" class="validate">
+            <label for="email">Phone Number</label>
           </div>
         </div>
 
--- login-sys.php
+++ login-sys.php
@@ -1,38 +1,53 @@
 <?php
+session_start();
+if(isset($_POST['username'])!=null)
+{
+$logusername=$_POST['username'];
+$logpassword=$_POST['password'];
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "aqua";
+// Establishing Connection with Server by passing server_name, user_id and password as a parameter
+$connection = mysql_connect($servername, $username, $password);
+// To protect MySQL injection for Security purpose
+$logusername = stripslashes($logusername);
+$logpassword = stripslashes($logpassword);
 
-$host="localhost"; // Host name 
-$username=""; // Mysql username 
-$password=""; // Mysql password 
-$db_name="test"; // Database name 
-$tbl_name="members"; // Table name 
+$logusername = mysql_real_escape_string($logusername);
+$logpassword = mysql_real_escape_string($logpassword);
 
-// Connect to server and select databse.
-mysql_connect("$host", "$username", "$password")or die("cannot connect"); 
-mysql_select_db("$db_name")or die("cannot select DB");
+// Selecting Database
+$db = mysql_select_db($dbname, $connection);
+// SQL query to fetch information of registerd users and finds user match.
 
-// username and password sent from form 
-$myusername=$_POST['myusername']; 
-$mypassword=$_POST['mypassword']; 
 
-// To protect MySQL injection (more detail about MySQL injection)
-$myusername = stripslashes($myusername);
-$mypassword = stripslashes($mypassword);
-$myusername = mysql_real_escape_string($myusername);
-$mypassword = mysql_real_escape_string($mypassword);
-$sql="SELECT * FROM $tbl_name WHERE username='$myusername' and password='$mypassword'";
-$result=mysql_query($sql);
 
-// Mysql_num_row is counting table row
-$count=mysql_num_rows($result);
+$query = mysql_query("select * from `user` where password='$logpassword' AND email='$logusername'", $connection);
+$rows = mysql_num_rows($query);
+if ($rows == 1) {
+$_SESSION['login_user']=$logusername; // Initializing Session
 
-// If result matched $myusername and $mypassword, table row must be 1 row
-if($count==1){
 
-// Register $myusername, $mypassword and redirect to file "login_success.php"
-session_register("myusername");
-header("location:home");
+	header("location: place_order.php");
+
+
+
+} else {
+header("location: index.php?msg=err");
 }
-else {
-echo "Wrong Username or Password";
+mysql_close($connection); // Closing Connection
 }
+else{
+	header("location: index.php?msg=err");
+}
+
+
+
+
+
+
+
+
+
 ?>
