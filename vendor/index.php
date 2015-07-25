f07afdd4ae7d0396fb73e60eb0ee23efd73cedd4
askprateek <askpk123@gmail.com>
2015-07-25 21:51:36 +0530

    vendor Wireframe finished

--- vendor/index.php
+++ vendor/index.php
@@ -1,56 +1,79 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Waterworks</title>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <title>Waterworks</title>
 
-    <!-- Bootstrap -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/css/materialize.min.css">
-    <link href="css/style.css" rel ="stylesheet">
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
+  <!-- Bootstrap -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/css/materialize.min.css">
+  <link href="css/style.css" rel ="stylesheet">
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+<body class="cyan">
 
   <nav>
-    <div class="nav-wrapper">
-      <a href="#" class="brand-logo center">Waterworks</a>
+    <div class="nav-wrapper white">
+      <a href="#" class="brand-logo center" style="color:#00BCD4;">Waterworks</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+       <li><a style="color:#00BCD4;">Hello Vendor here</a></li>
+     </ul>
     </div>
   </nav>
 
-    <div class="container">
-    <div class="row card-panel blue lighten-5">
-      <div class="col s3">
-        <h4> Hello Vendor</h4>
+  <div class="container">
+    <div class="row">
+      <div class="col s6">
+        <table class="bordered white" style ="margin-top:10px;border-radius:3px;">
+          <thead>
+            <tr>
+              <th data-field="pid">Product</th>
+              <th data-field="quantity">Quantity</th>
+              <th data-field="units">Units</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>Bottle</td>
+              <td>5 ltr</td>
+              <td>6</td>
+            </tr>
+            <tr>
+              <td>Tanker</td>
+              <td>200 ltr</td>
+              <td>3</td>
+            </tr>
+          </tbody>
+        </table>
+
+
+
       </div>
-      <div class="col s9">
-        <h4> ZIPCODE HERE</h4>
-      <div>
-    </div>
-  </div>
-</div>
-  <div class="row">
-    <div class="col s6">
-        Table: His Products
-    </div>
 
-    <div class="col s6">
-        Notifications
-    </div>
+      <div class="col s6">
 
-  </div>
+        <div class="collection">
+          <a href="#!" class="collection-item">Alvin</a>
+          <a href="#!" class="collection-item">Alvin</a>
+          <a href="#!" class="collection-item">Alvin</a>
+          <a href="#!" class="collection-item">Alvin</a>
+        </div>
+      </div>
+
+    </div>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/js/materialize.min.js"></script>
     <script src="js/script.js"></script>
   </body>
-</html>
+  </html>
