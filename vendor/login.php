457c6b8e53e0e4fddfecb26d4c71866b25edcf18
askprateek <askpk123@gmail.com>
2015-07-25 22:30:22 +0530

    login form added

---
+++ vendor/login.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>AQUA</title>
+
+    <!-- Bootstrap -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/css/materialize.min.css">
+    <link href="css/style.css" rel ="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	<!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/css/materialize.min.css">
+
+  <!-- Compiled and minified JavaScript -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/js/materialize.min.js"></script>
+
+  </head>
+  <body class="cyan">
+  <nav>
+    <div class="nav-wrapper white">
+      <a href="#" class="brand-logo center" style="color:#00BCD4;">AQUA</a>
+    </div>
+  </nav>
+
+    <div class="container">
+
+      <form action="login-sys.php" method="post">
+	  <div class="card-panel">
+      <span class="blue-text text-darken-2"><h5>Vendor Login</h5></span>
+
+	  	<div class="row">
+        <div class="input-field col s12">
+          <input id="email" type="email" name="email" class="validate">
+          <label for="email">Email</label>
+        </div>
+      </div>
+
+	    <div class="row">
+        <div class="input-field col s12">
+          <input id="email" type="password" name="pass" class="validate">
+          <label for="email">Password</label>
+        </div>
+      </div>
+	 <button type="submit" class="waves-effect waves-light btn">Login</button>
+ </div>
+	  </form>
+
+     <script type="text/javascript" src="http://theasp.tk/js/jquery-1.11.1.min.js"></script>
+ <!-- Modal Trigger -->
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/js/materialize.min.js"></script>
+    <script src="js/script.js"></script>
+  </body>
+</html>
